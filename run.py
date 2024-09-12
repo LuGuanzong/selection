5aab18914eb5b5838ecd0db5155eb4d69c35db97
luguanzong-git <746722756@qq.com>
2024-09-12 19:54:38 +0800

    update：本地搞了个测试数据库

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import os
 
 from app.factory import create_app
 
-config_dict = dict(development='DEVELOPMENT', production='PRODUCTION')
-env = os.environ.get('FLASK_ENV', 'development')
+# config_dict = dict(development='DEVELOPMENT', production='PRODUCTION')
+# env = os.environ.get('FLASK_ENV', 'development')
 
-app = create_app(config_name=config_dict[env])
+app = create_app(config_name='PRODUCTION')
 
 if __name__ == "__main__":
     app.run()
