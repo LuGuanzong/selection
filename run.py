6edb5441330b7012dd91a1655f6dd18e884bfed5
luguanzong-git <746722756@qq.com>
2024-09-12 19:38:19 +0800

    update：代码优化

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 
 from app.factory import create_app
 
-config_dict = dict(devlopment='DEVELOPMENT', production='PRODUCTION')
-env = os.environ.get('FLASK_ENV', 'devlopment')
+config_dict = dict(development='DEVELOPMENT', production='PRODUCTION')
+env = os.environ.get('FLASK_ENV', 'development')
 
 app = create_app(config_name=config_dict[env])
 
